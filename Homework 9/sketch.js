62fb6bd3f74cbcf369127da9623a97b7693aacaa
LexiNunnally627 <agn2004@icloud.com>
2024-10-28 22:59:06 -0600

    Homework 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,34 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+function draw()
+{
+    background(120);
+    strokeWeight(7);
+    fill(2256,250,150);
+    ellipse(250,430,300,500);
+    fill(200,150,100);
+    strokeWeight(7);
+    rect(200,350,100,150);
+    ellipse(150,300,50,75);
+    ellipse(350,300,50,75);
+    fill(250,100,150);
+    circle(250,625,350);
+    fill(200,150,100);
+    circle(250,300,200);
+    fill(100,100,100);
+    square(175,275,50);
+    square(275,275,50);
+    line(225,300,275,300);
+    triangle(250,330,250,320,245,330);
+    line(275,350,250,350);
+    strokeWeight(13);
+    point(200,300);
+    point(300,300);
+    fill(300,200,450);
+    textSize(15);
+    text('Lexi Nunnally',400,550);
+    textSize(20);
+    text('My Self Portrait',175,100);
+}
